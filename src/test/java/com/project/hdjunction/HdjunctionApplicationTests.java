ae1d9a9e7b5bc1194d670540ced576df68edccf3
leechangtaek <ckdxor1014@naver.com>
2023-06-01 17:19:26 +0900

    웹 프로젝트 구성완료

---
+++ src/test/java/com/project/hdjunction/HdjunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hdjunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdjunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
